0a97df4887e0ce5f21573d6e3fb0cb1c39941a1e
vadim-levotsky <vadim.levotsky@hotmail.com>
2024-11-27 16:17:29 +0100

    add lists

--- lists.py
+++ lists.py
@@ -26,6 +26,36 @@ motorcycles.insert(2, 'HONDA')
 print(f"my garage: {motorcycles}")
 
 motorcycles.remove("honda".upper()) ## remove only 1st occurrence of the value
-print(f"my garage: {sorted(motorcycles)}")
+print(f"my garage: {sorted(motorcycles)}")  ## function 'sorted(list)' sort list without modifying it
+
+## List could hold mix different types.
+## but sort would not work because of different types
+items = ["Roger", 1, "syd", True]
+
+print("Roger" in items) # -> true
+print("Jim" in items) # -> false
+
+print(items[1])
+# update item
+items.append("Jim")
+items.extend(["bob", 5])
+print(items[2:4]) # -> ['Syd', True]
+print(items[4:])  # -> ['Jim', 'Bob', 5]
+
+items += ["Beau", "milky"]
+
+## make a copy
+print("\n**** COPY ****")
+items_copy = items[:] ## make a slice with nothing in the beginning and nothing in the end
+print(f'items copy: {items_copy}')
+items_copy = [str(element) for element in items_copy]
+print("\n**** SORT WITHOUT MODIFYING LIST****")
+sorted_list = sorted(items_copy, key=lambda x: x.lower())
+print(f"original list: {items_copy}\nsorted list: {sorted_list}")
+sorted_list = sorted(items_copy, key=str.lower, reverse=True)
+print(f"original list: {items_copy}\nreverse sorted list: {sorted_list}")
+print("\n**** SORT WIT MODIFYING LIST****")
+items_copy.sort(key=str.lower)
+print(f"sorted items: {items_copy}")
 
 
